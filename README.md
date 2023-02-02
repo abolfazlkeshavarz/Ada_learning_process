3789bfa01399ba80db4fb1187d8c0a9167f20889
Abolfazl <abolfazlkeshavarz1112@gmail.com>
2023-02-02 22:17:31 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ada_Learning_myself_Codes
+ 
---
+++ src/main.adb
@@ -0,0 +1,8 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+
+begin
+   Put_Line("Hello everyone");
+   Put_Line("Who are You?");
+end Main;
